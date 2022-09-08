e10e5c5ab39b2943cdd3442944ced9eaa645c096
Lucas Rodrigues Pereira <lucasrpereira29@gmail.com>
2022-09-08 17:11:38 -0300

    Update matriz elevada ao quadrado.c
    agora o usuário escolhe o valor da potencia

--- matriz elevada ao quadrado.c
+++ matriz elevada ao quadrado.c
@@ -3,17 +3,20 @@
 
 int main()
 {
-    int vetor[5],vetor2[5],x;
+    int vetor[5],vetor2[5],x,potencia;
     
       for(x = 0; x<5; x++)
     {
       printf("5 numeros");
       scanf("%d",&vetor[x]);
     }
-    
+
+     printf("Deseja elevar a qual potencia?");
+     scanf("%d",&potencia);
+
      for (x=0;x<5;x++)
     {
-        vetor2[x] = pow(vetor[x],2);
+        vetor2[x] = pow(vetor[x],potencia);
     } 
     for (x=0;x<5;x++){
         
